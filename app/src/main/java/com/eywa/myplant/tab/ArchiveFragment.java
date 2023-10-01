e762b7a0f13454daa0e490b2bad7486fbaf0516d
Hangyeol-SSeo <dongha313@gmail.com>
2023-10-02 01:46:00 +0900

    feat: Archive 사진 및 링크 추가

--- app/src/main/java/com/eywa/myplant/tab/ArchiveFragment.java
+++ app/src/main/java/com/eywa/myplant/tab/ArchiveFragment.java
@@ -10,6 +10,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +29,8 @@ import com.eywa.myplant.tab.placeholder.ArchiveHolderContent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class ArchiveFragment extends Fragment implements OnBackPressedListener {
     public static WebView webView;
@@ -34,6 +38,10 @@ public class ArchiveFragment extends Fragment implements OnBackPressedListener {
     private SearchView searchView;
     private RecyclerView archiveCategoryRecyclerView;
     private ArchiveCategoryRecyclerViewAdapter archiveCategoryRecyclerViewAdapter;
+
+    private final Executor executor = Executors.newSingleThreadExecutor(); // 백그라운드 작업을 위한 Executor
+    private final Handler handler = new Handler(Looper.getMainLooper()); // 메인 스레드에서 UI 업데이트를 위한 Handler
+
 
 
     @Override
@@ -67,9 +75,7 @@ public class ArchiveFragment extends Fragment implements OnBackPressedListener {
             }
         });
 
-        // Assume getArchiveCategories() returns a list of ArchiveCategoryHolderContent
-        archiveCategoryRecyclerViewAdapter = createArchiveInstance();
-        archiveCategoryRecyclerView.setAdapter(archiveCategoryRecyclerViewAdapter);
+        executor.execute(new CreateArchiveInstanceRunnable());
 
         return view;
     }
@@ -89,17 +95,68 @@ public class ArchiveFragment extends Fragment implements OnBackPressedListener {
         ((MainActivity) context).setOnBackPressedListener(this);
     }
 
+    private class CreateArchiveInstanceRunnable implements Runnable {
+        @Override
+        public void run() {
+            final ArchiveCategoryRecyclerViewAdapter archiveCategoryRecyclerViewAdapter = createArchiveInstance();
+
+            // 메인 스레드에서 RecyclerView에 Adapter를 설정
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    archiveCategoryRecyclerView.setAdapter(archiveCategoryRecyclerViewAdapter);
+                }
+            });
+        }
+    }
+
     public ArchiveCategoryRecyclerViewAdapter createArchiveInstance() {
         // 기초 도감 item instance 생성
-        List<ArchiveHolderContent> archiveHolderContentList = new ArrayList<>();
-        archiveHolderContentList.add(new ArchiveHolderContent(Uri.parse("https://nongsaro.go.kr/cms_contents/301/12976_MF_ATTACH_01.jpg"), Uri.parse("https://nongsaro.go.kr/portal/ps/psz/psza/contentSub.ps?menuId=PS00376&cntntsNo=12976&totalSearchYn=Y"), "식물이름1", "종1", "학명1"));  // 기초 도감 item을 Recyclerview에 추가
-        ArchiveRecyclerViewAdapter recyclerViewAdapter = new ArchiveRecyclerViewAdapter(archiveHolderContentList);
+        // 가
+        List<ArchiveHolderContent> archiveHolderContentList1 = new ArrayList<>(); // 기초 도감 item을 Recyclerview에 추가
+        archiveHolderContentList1.add(new ArchiveHolderContent(Uri.parse("https://www.nongsaro.go.kr/cms_contents/301/15831_MF_REPR_ATTACH_01.jpg"), Uri.parse("https://www.nongsaro.go.kr/portal/ps/psz/psza/contentSub.ps;jsessionid=mVwOCiVxmY2YXTJtc1547ZsGsGM9OcMnUawn1naa1X1o9nllU8raxbz1eSRN0wBW.nongsaro-web_servlet_engine1?menuId=PS00376&cntntsNo=15831&totalSearchYn=Y"), "고무나무", "뽕나무과", "Ficus benghalensis", 16, 28, 40, 70, 3, 4, 1));
+        ArchiveRecyclerViewAdapter recyclerViewAdapter1 = new ArchiveRecyclerViewAdapter(archiveHolderContentList1);
+
+        // 사
+        List<ArchiveHolderContent> archiveHolderContentList7 = new ArrayList<>(); // 기초 도감 item을 Recyclerview에 추가
+        archiveHolderContentList7.add(new ArchiveHolderContent(Uri.parse("https://nongsaro.go.kr/cms_contents/301/12976_MF_ATTACH_01.jpg"), Uri.parse("https://nongsaro.go.kr/portal/ps/psz/psza/contentSub.ps?menuId=PS00376&cntntsNo=12976&totalSearchYn=Y"), "스킨답서스", "천남성과", "Epipremnum aureum 'Lime'", 21, 25, 40, 70, 2, 4, 1));
+        archiveHolderContentList7.add(new ArchiveHolderContent(Uri.parse("https://www.nongsaro.go.kr/cms_contents/1122/204869_MF_BIMG_01.jpg"), Uri.parse("https://www.nongsaro.go.kr/portal/ps/psz/psza/contentMain.ps?menuId=PS04099&gnbIndex=CUSTOMINFO&directUrl=http%3A%2F%2Fwww.nongsaro.go.kr%2Fportal%2Fps%2Fpsz%2Fpsza%2FcontentMain.ps%3FmenuId%3DPS04099&sServiceCd=1410&lServiceCd=1006&mServiceCd=1045&contentId=15183&pageUnit=6"), "산세베리아 하니", "용설란과", "Sansevieria trifasciata 'Hahnii'", 18, 27, 40, 70, 2, 4, 15));
+        archiveHolderContentList7.add(new ArchiveHolderContent(Uri.parse("https://www.nongsaro.go.kr/cms_contents/301/19717_MF_REPR_ATTACH_01.jpg"), Uri.parse("https://www.nongsaro.go.kr/portal/ps/psz/psza/contentSub.ps;jsessionid=b11ckNmOQAJkfRjXMm1C3xMFL1MV6pVCN5WTo0CVochQWS9nUeGdFhlAdi9rmVVl.nongsaro-web_servlet_engine1?menuId=PS00376&cntntsNo=19717&totalSearchYn=Y"), "스파티필룸", "천남성과", "Spathiphyllum wallisii", 18, 32, 40, 70, 2, 3, 3));
+        ArchiveRecyclerViewAdapter recyclerViewAdapter7 = new ArchiveRecyclerViewAdapter(archiveHolderContentList7);
+
+        // 아
+        List<ArchiveHolderContent> archiveHolderContentList8 = new ArrayList<>(); // 기초 도감 item을 Recyclerview에 추가
+        archiveHolderContentList8.add(new ArchiveHolderContent(Uri.parse("https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/cg7o/image/gezw1ErBCRBHLIVHHxJ6-HDUfWg.jpg"), Uri.parse("https://fuleaf.com/plants/detail/5ecb98b8612274031566b2d3"), "안스리움", "천남성과", "Anthurium andraeanum", 18, 25, 40, 70, 2, 3, 3));
+        archiveHolderContentList8.add(new ArchiveHolderContent(Uri.parse("https://www.nongsaro.go.kr/cms_contents/301/18658_MF_REPR_ATTACH_01.jpg"), Uri.parse("https://www.nongsaro.go.kr/portal/ps/psz/psza/contentSub.ps?menuId=PS00376&cntntsNo=18658&totalSearchYn=Y"), "알로카시아", "천남성과", "Alocasia amazonica", 21, 25, 40, 70, 2, 3, 3));
+        ArchiveRecyclerViewAdapter recyclerViewAdapter8 = new ArchiveRecyclerViewAdapter(archiveHolderContentList8);
+
+        // 자
+        List<ArchiveHolderContent> archiveHolderContentList9 = new ArrayList<>(); // 기초 도감 item을 Recyclerview에 추가
+        archiveHolderContentList9.add(new ArchiveHolderContent(Uri.parse("https://www.nongsaro.go.kr/cms_contents/301/16447_MF_REPR_ATTACH_01.jpg"), Uri.parse("https://www.nongsaro.go.kr/portal/ps/psz/psza/contentMain.ps?menuId=PS00376&pageIndex=1&pageSize=10&pageUnit=10&cntntsNo=&detailSrch=&sDtlSrchText=%E3%85%88&sType=sCntntsSj&sText=&sDtlSrchType=sDtlSrchsCntntsSj&sDtlSrchLight=&sDtlSrchGrwhstle=&sDtlSrchLefcolr=&sDtlSrchLefmrk=&sDtlSrchFlclr=&sDtlSrchFmldecolr=&sDtlSrchIgnSeason=&sDtlSrchWinterLwet=&priceType=&sDtlSrchPriceType=&sDtlSrchWaterCycle="), "접란", "백합과", "Chlorophytum comosum", 16, 28, 40, 70, 2, 4, 3));
+        ArchiveRecyclerViewAdapter recyclerViewAdapter9 = new ArchiveRecyclerViewAdapter(archiveHolderContentList9);
+
+        // 타
+        List<ArchiveHolderContent> archiveHolderContentList12 = new ArrayList<>(); // 기초 도감 item을 Recyclerview에 추가
+        archiveHolderContentList12.add(new ArchiveHolderContent(Uri.parse("https://www.nongsaro.go.kr/cms_contents/301/13002_MF_REPR_ATTACH_01.jpg"), Uri.parse("https://www.nongsaro.go.kr/portal/ps/psz/psza/contentSub.ps?menuId=PS00376&pageIndex=1&pageSize=10&pageUnit=6&cntntsNo=13002&detailSrch=&sDtlSrchText=%E3%85%8C&sType=sCntntsSj&sText=&sDtlSrchType=sDtlSrchsCntntsSj&sDtlSrchLight=&sDtlSrchGrwhstle=&sDtlSrchLefcolr=&sDtlSrchLefmrk=&sDtlSrchFlclr=&sDtlSrchFmldecolr=&sDtlSrchIgnSeason=&sDtlSrchWinterLwet=&priceType=big&sDtlSrchPriceType=&sDtlSrchWaterCycle="), "테이블야자", "야자과", "Chamaedorea elegans", 21, 25, 40, 70, 2, 4, 3));
+        ArchiveRecyclerViewAdapter recyclerViewAdapter12 = new ArchiveRecyclerViewAdapter(archiveHolderContentList12);
+
+        // 하
+        List<ArchiveHolderContent> archiveHolderContentList14 = new ArrayList<>(); // 기초 도감 item을 Recyclerview에 추가
+        archiveHolderContentList14.add(new ArchiveHolderContent(Uri.parse("https://www.nongsaro.go.kr/cms_contents/301/12986_MF_REPR_ATTACH_01.jpg"), Uri.parse("https://www.nongsaro.go.kr/portal/ps/psz/psza/contentSub.ps?menuId=PS00376&cntntsNo=12986&totalSearchYn=Y"), "호야", "박주가리과", "Hoya carnosa", 16, 27, 40, 70, 6, 12, 15));
+        archiveHolderContentList14.add(new ArchiveHolderContent(Uri.parse("https://www.nongsaro.go.kr/cms_contents/301/19453_MF_REPR_ATTACH_01.jpg"), Uri.parse("https://www.nongsaro.go.kr/portal/ps/psz/psza/contentSub.ps?menuId=PS00376&cntntsNo=19453&totalSearchYn=Y"), "후마타 고사리", "고사리과", "Humata tyermannii", 16, 20, 70, 100, 2, 4, 3));
+        ArchiveRecyclerViewAdapter recyclerViewAdapter14 = new ArchiveRecyclerViewAdapter(archiveHolderContentList14);
 
         // category item 추가
-        List<ArchiveCategoryHolderContent> categoryHolderContentList = new ArrayList<>();
-        categoryHolderContentList.add(new ArchiveCategoryHolderContent("ㄱ", recyclerViewAdapter));  // category item을 Recyclerview에 추가
+        List<ArchiveCategoryHolderContent> categoryHolderContentList = new ArrayList<>(); // category item을 Recyclerview에 추가
+        categoryHolderContentList.add(new ArchiveCategoryHolderContent("ㄱ", recyclerViewAdapter1));
+        categoryHolderContentList.add(new ArchiveCategoryHolderContent("ㅅ", recyclerViewAdapter7));
+        categoryHolderContentList.add(new ArchiveCategoryHolderContent("ㅇ", recyclerViewAdapter8));
+        categoryHolderContentList.add(new ArchiveCategoryHolderContent("ㅈ", recyclerViewAdapter9));
+        categoryHolderContentList.add(new ArchiveCategoryHolderContent("ㅌ", recyclerViewAdapter12));
+        categoryHolderContentList.add(new ArchiveCategoryHolderContent("ㅎ", recyclerViewAdapter14));
 
         ArchiveCategoryRecyclerViewAdapter archiveCategoryRecyclerViewAdapter = new ArchiveCategoryRecyclerViewAdapter(categoryHolderContentList);
         return archiveCategoryRecyclerViewAdapter;
     }
+
 }
--- app/src/main/java/com/eywa/myplant/tab/placeholder/ArchiveHolderContent.java
+++ app/src/main/java/com/eywa/myplant/tab/placeholder/ArchiveHolderContent.java
@@ -8,13 +8,29 @@ public class ArchiveHolderContent {
     public final String plantNameKor;
     public final String species;
     public final String scientificName;
-    // 습도, 온도 등 조건 추가
 
-    public ArchiveHolderContent (Uri imageUri, Uri pageUri, String plantNameKor, String species, String scientificName) {
+    // 물, 조도, 일조량, 습도
+    public final int temperature_min, temperature_max; // 생육 온도 최소 / 최대 (단위: 섭씨)
+    public final int humidity_min, humidity_max; // 습도 최소 / 최대 (단위: %)
+    public final int light_intensity_min, light_intensity_max; // 일조량 최소 / 최대 (단위: 시간)
+    public final int soil_moisture_limit; // 물을 줘야 할 때 (흙이 마른 정도) (단위: cm)
+
+    public ArchiveHolderContent (Uri imageUri, Uri pageUri, String plantNameKor, String species, String scientificName,
+                                 int temperature_min, int temperature_max,
+                                 int humidity_min, int humidity_max,
+                                 int light_intensity_min, int light_intensity_max, int soil_moisture_limit) {
         this.imageUri = imageUri;
         this.pageUri = pageUri;
         this.plantNameKor = plantNameKor;
         this.species = species;
         this.scientificName = scientificName;
+
+        this.temperature_min = temperature_min;
+        this.temperature_max = temperature_max;
+        this.humidity_min = humidity_min;
+        this.humidity_max = humidity_max;
+        this.light_intensity_min = light_intensity_min;
+        this.light_intensity_max = light_intensity_max;
+        this.soil_moisture_limit = soil_moisture_limit;
     }
 }
